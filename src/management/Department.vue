a18f90205caeb4f41884339f491d443b81f5d342
Niwsei <loveonelove2004@gmail.com>
2024-02-03 16:56:29 +0700

    3/2/2024

--- src/management/Department.vue
+++ src/management/Department.vue
@@ -1,197 +1,206 @@
 <template>
-    <div>
-        <!-- card -->
-    <v-card height="300" >
-         <v-card-title>
-            <h3 class="font-weight-regular mt-5 ml-5">ສ້າງພະເເນກ</h3>
+  <div>
+    <!-- card -->
+    <v-card height="300">
+      <v-card-title>
+        <h3 class="font-weight-regular mt-5 ml-5">ສ້າງພະເເນກ</h3>
+      </v-card-title>
 
-        </v-card-title> 
-
-            <v-card-text  class="py-10 px-5">
-              {{ JSON.stringify(forms) }}
-        <label style="font-size: 16px;font-weight: bold;" >ຊື່ພະເເນກ</label>
+      <v-card-text class="py-10 px-5">
+        <label style="font-size: 16px; font-weight: bold">ຊື່ພະເເນກ</label>
         <v-form validate-on="submit lazy" @submit.prevent="submitForm">
           <v-text-field
             v-model.trim="forms.name"
-            class="border border-1 border-primary"
-            color="border-primary"
-            required
-            variant="outlined"
+            class="border border-1 border-deep-purple-lighten-2"
+            variant
+            density="compact"
             hide-details
           ></v-text-field>
           <div class="d-flex justify-end px-10 py-10">
-      <v-btn rounded
-      text="submit"
-      type="submit"
-      variant="tonal"
-      size="large"
-      class="bg-thirteen"
-        >
-        
-        ສ້າງພະເເນກ
-      </v-btn>
-    </div>
+            <v-btn
+              rounded
+              text="submit"
+              type="submit"
+              size="large"
+              class="text-blue-blue-grey-lighten-5"
+              color="deep-orange-accent-3"
+            >
+              ສ້າງພະເເນກ
+            </v-btn>
+          </div>
         </v-form>
-        
-     </v-card-text>
+      </v-card-text>
     </v-card>
 
-
-
-
-
-
-
     <!-- table -->
-
-    <v-table density="compact" height="auto" class="mt-15 pt-5">
-    <thead>
-      <tr>
-        <th class="text-left">
-          ເລກທີ
-        </th>
-        <th class="text-left">
-          ພະເເນກ
-        </th>
-        <th class="text-left">
-          
-        </th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr
-        v-for="(item , index) in forms"
-        :key="index"
-      >
-        <td> {{index}} </td>
-        <td>{{item.name}}</td>
-        <td>
-            <v-btn icon flat>
-       
-      </v-btn>
-      <v-menu>
-      <template v-slot:activator="{ props }">
-        <v-btn icon flat
-          dark
-          v-bind="props"
-        >
-        <v-icon extended>mdi-dots-vertical</v-icon>
-        </v-btn>
-      </template>
-
-      <v-list>
-        
-       
-    <v-dialog
-      v-model="dialog"
-      persistent
-      width="1024"
-    >
-      <template v-slot:activator="{ props }">
-        <v-btn
-          flat
-          v-bind="props"
-        >
-          ເເກ້ໄຂ
-        </v-btn>
-      </template>
-      <v-card>
-        <v-card-title>
-          <span class="text-h5">User Profile</span>
-        </v-card-title>
-        <v-card-text>
-          <v-container>
-            <v-row>
-             
-              <v-col cols="12">
-                <v-text-field
-                  label="Email*"
-                  required
-                ></v-text-field>
-              </v-col>
-              
-            </v-row>
-          </v-container>
-          <small>*indicates required field</small>
-        </v-card-text>
-        <v-card-actions>
-
-          <v-btn
-            color="blue-darken-1"
-            variant="text"
-            @click="dialog = false"
-          >
-            Save
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
-  
+    <v-card height="auto" class="mt-15 ">
+      <v-container>
+    <v-table class="mt-15 border border-left-0 border-right-none" theme="">
+      <thead>
+        <tr>
+          <th class="text-left font-weight-bold pl-15">ເລກທີ</th>
+          <th class="text-left font-weight-bold pl-15">ພະເເນກ</th>
+          <th class="text-left"></th>
+        </tr>
+      </thead>
 
 
-            
-            <v-list-item>  ລືບ  </v-list-item>
+      <tbody>
+        <tr v-for="(item, index) in dessert" :key="index">
+          <td>{{ item.number }}</td>
+          <td>{{ item.name }}</td>
+          <td>
+            <v-menu>
+              <template v-slot:activator="{ props }">
+                <v-btn icon flat dark v-bind="props">
+                  <v-icon extended>mdi-dots-vertical</v-icon>
+                </v-btn>
+              </template>
 
-      </v-list>
-    </v-menu>
-        </td>
-      </tr>
-    </tbody>
-  </v-table>
+              <v-list>
+                <v-dialog  v-model="dialog" persistent width="1024">
+                  <template v-slot:activator="{ props }">
+                    <v-btn flat v-bind="props"  @click="editItem(index)"> ເເກ້ໄຂ </v-btn>
+                  </template>
+                  <v-card>
+                    <v-card-title>
+                      <span class="text-h5">User Profile</span>
+                    </v-card-title>
+                    <v-card-text>
+                      <v-container>
+                        <v-row>
+                          <v-col cols="12">
+                            <v-text-field
+                            v-model="editedItem.name"
+                              required
+                            ></v-text-field>
+                          </v-col>
+                        </v-row>
+                      </v-container>
+                      <small>*indicates required field</small>
+                    </v-card-text>
+                    <v-card-actions>
+                      <v-btn
+                        color="blue-darken-1"
+                        variant="text"
+                        @click="close"
+                      >
+                        ເເກ
+                      </v-btn>
+                    </v-card-actions>
+                  </v-card>
+                </v-dialog>
 
+                <!-- <v-list-item> ລືບ </v-list-item> -->
 
-
-    </div>
+                <v-dialog v-model="dialogs" persistent width="1024">
+                  <template v-slot:activator="{ props }">
+                    <v-btn flat v-bind="props">
+                      ລືບ
+                    </v-btn>
+                  </template>
+                  <v-card>
+                    <v-card-title>
+                      <span class="text-h5">User Profile</span>
+                    </v-card-title>
+                    <v-card-text>
+                      <v-container>
+                        <v-row>
+                          <v-col cols="12">
+                            <v-text-field
+                              label="Email*"
+                              required
+                            ></v-text-field>
+                          </v-col>
+                        </v-row>
+                      </v-container>
+                      <small>*indicates required field</small>
+                    </v-card-text>
+                    <v-card-actions>
+                      <v-btn
+                        color="blue-darken-1"
+                        variant="text"
+                        @click="dialog = false"
+                      >
+                        Save
+                      </v-btn>
+                    </v-card-actions>
+                  </v-card>
+                </v-dialog>
+              </v-list>
+            </v-menu>
+          </td>
+        </tr>
+      </tbody>
+    </v-table>
+  </v-container>
+  </v-card>
+  </div>
 </template>
 
 <script>
 // import test from '../alert/test.vue'
 export default {
-    // components:{test},
+  // components:{test},
 
-    
-
-    data() {
-        return {
-
-         dessert:{
-            name:"siyon" , lname:"sithongloun", age:"20" ,
-         },
-
-         dialog: false,
-
-         forms:{
-       name:""
-      },
-      
-       employee:[],
-        }
+  data() {
+    return {
+      dessert: {
+        number: "0",
+        name: "siyon",
         
-    },
-    methods: {
+      },
+      defaultdessert:[
+        {
+        name: "",
+        lname: "",
+       
+      },
+    ],
 
+      editedItem: {
+      name: "", // สามารถกำหนดค่าเริ่มต้นที่ต้องการแสดงในฟอร์ม
+    },
+
+      dialog: false,
+      
+      
+      editedIndex: -1,
+
+      forms: {
+        name: "",
+      },
+
+      employee: [],
+    };
+  },
+  methods: {
     //  oninput(event) {
     //  this.name = event.target.value;
     //  },
 
-      submitForm(event) {
+    submitForm(event) {
       this.employee = event.target.value;
-      console.log(this.employee)
+      console.log(this.employee);
 
-
-
-
-        // this.loading = true;
-        // const results = await event;
-        // this.loading = false;
-
+      // this.loading = true;
+      // const results = await event;
+      // this.loading = false;
+    },
+    close(){
+        this.dialog = false;
+        this.$nextTick(() => {
+          this.dessert.name = Object.assign({},this.defaultdessert)
+        })
       },
 
-
-    },
-}
+      editItem(index) {
+  this.editedIndex = index;
+  this.editedItem = Object.assign({}, this.forms[index]);
+  this.dialog = true; // เปิด v-dialog
+},
+  },
+};
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
--- src/management/FontFunction.vue
+++ src/management/FontFunction.vue
@@ -1,85 +1,98 @@
 <template>
-    <div>
-<h1>ຫນ້າຟັງຊັນ</h1>
-<v-card height="550" >
+  <div>
+    <h1>ຫນ້າຟັງຊັນ</h1>
+    <v-card height="550">
+      <v-card-title>ຈັດການຫນ້າຟັງຊັ້ນ</v-card-title>
 
-<v-card-title>ຈັດການຫນ້າຟັງຊັ້ນ</v-card-title> 
-
-
-   <v-card-text  class="py-10 px-5">
-<form @submit.prevent="submit">
-
-    <label for="" class="font-weight-black">ຫົວຂໍ້</label>
-    <v-select
-
-label="Select"
-></v-select>
-
-<label for="" class="font-weight-black">ຊື່ຫນ້າ</label>
-    <v-text-field
-      label="Name"
-    ></v-text-field>
-
-    <label for="" class="font-weight-black">ຊື່ໄຟຣ</label>
-    <v-text-field
-
-      label="E-mail"
-    ></v-text-field>
+      <v-card-text class="py-10 px-5">
+        <form @submit.prevent="submit">
+          <div>
+            <label for="" class="font-weight-black">ຫົວຂໍ້</label>
+            <v-select
+              :items="section"
+              class="border border-1"
+              density="compact"
+              hide-details
+              label="ເລືອກຫົວຂໍ້"
+            ></v-select>
+          </div>
 
 
+          <div class="my-10">
+            <label for="" class="font-weight-black">ຊື່ຫນ້າ</label>
+            <v-text-field
+              class="border border-1"
+              hide-details
+              variant
+              density="compact"
+            ></v-text-field>
+          </div>
 
-    <div class="d-flex justify-end px-10 py-10">
-<v-btn rounded
-type="submit"
-color="blue" >
-ສ້າງພະເເນກ
-</v-btn>
-</div>
 
-  </form>
-</v-card-text>
+          <div>
+            <label for="" class="font-weight-black">ຊື່ໄຟຣ</label>
+            <v-text-field
+              class="border border-1"
+              hide-details
+              variant
+              density="compact"
+            ></v-text-field>
+          </div>
 
-</v-card>
-   </div>
 
-   <v-table density="compact" height="auto" class="mt-15">
+          <div class="d-flex justify-end px-10 py-10">
+            <v-btn
+              rounded
+              size="large"
+              type="submit"
+              class="text-blue-blue-grey-lighten-5"
+              color="deep-orange-accent-3"
+            >
+              ເພີ່ມຂໍ້ມູນ
+            </v-btn>
+          </div>
+        </form>
+      </v-card-text>
+    </v-card>
+  </div>
+
+  
+  <v-table density="compact" height="auto" class="mt-15">
     <thead>
       <tr>
-        <th class="text-left">
-          ເລກລຳດັບ
-        </th>
-        <th class="text-left">
-          ຊື່ຫນ້າ
-        </th>
-        <th class="text-left">
-         ຊື່ໄຟຣ
-        </th>
-        <th class="text-left">
-        ຫົວຂໍ້
-        </th>
-        <th class="text-left">
-
-        </th>
+        <th class="text-left">ເລກລຳດັບ</th>
+        <th class="text-left">ຊື່ຫນ້າ</th>
+        <th class="text-left">ຊື່ໄຟຣ</th>
+        <th class="text-left">ຫົວຂໍ້</th>
+        <th class="text-left"></th>
       </tr>
     </thead>
     <tbody>
-      <tr
-        v-for="item in desserts"
-        :key="item.name"
-      >
+      <tr v-for="item in desserts" :key="item.name">
         <td>{{ item.name }}</td>
         <td>{{ item.calories }}</td>
         <td>{{ item.calories }}</td>
       </tr>
     </tbody>
   </v-table>
-
 </template>
 
-<script setup>
-
+<script>
+export default {
+  data() {
+    return {
+      section: [
+        "ເລືອກຫົວຂໍ້",
+        "ຈັດການຂໍ້ມູນ",
+        "ຈັດການຄຳຂໍ",
+        "ສິນເຊື່ືອ",
+        "ການເງີນ",
+        "ກົດໝາຍ",
+        "ລາຍງານ",
+      ],
+    };
+  },
+};
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
